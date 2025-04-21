da57b4e73c8c3930600d70497f566775b612cb18
Documenter.jl <documenter@juliadocs.github.io>
2025-04-21 20:20:50 +0000

    build based on 91cee3e

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#StatisticalProjections.jl","page":"StatisticalProjections.jl","title":"StatisticalProjections.jl","text":"","category":"section"},{"location":"","page":"StatisticalProjections.jl","title":"StatisticalProjections.jl","text":"Documentation for StatisticalProjections.jl","category":"page"}]
+}
